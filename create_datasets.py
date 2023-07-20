d7d5e205cdfa780d938c86b830d7287d994c8b01
Dima <zhylko.dima@gmail.com>
2023-07-20 19:45:46 +0200

    dataset creation

---
+++ create_datasets.py
@@ -0,0 +1,68 @@
+import sys
+import os
+import time
+
+import pickle as pkl
+import numpy as np
+import h5py
+from tqdm import tqdm
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    os.makedirs('sparse', exist_ok=True)
+    os.makedirs('dense', exist_ok=True)
+
+    num_examples = int(sys.argv[1])
+    H = W = 420
+    focal_plains = 7
+    seq_len_range = (200, 600)
+
+    write_sparse = bool(sys.argv[2])
+    chunk_size = tuple(map(int, sys.argv[3:]))
+    if not chunk_size:
+        chunk_size = (1, focal_plains, H, W)
+
+    sparse_writes = []
+    dense_writes = []
+
+    with h5py.File(f'dense/data.hdf5', 'w') as d:
+        dataset = d.create_dataset('images',
+                                   shape=(0, focal_plains, H, W),
+                                   maxshape=(None, focal_plains, H, W),
+                                   chunks=chunk_size)
+
+        offset = 0
+        cum_seq_lens = []
+
+        for image_id in tqdm(range(num_examples)):
+            seq_len = int(np.random.normal(loc=sum(seq_len_range)/2, scale=0.5))
+            random_image_sequence = np.random.randint(0, 255, size=(seq_len, focal_plains, H, W), dtype=np.uint8)
+            offset += seq_len
+            cum_seq_lens.append(offset)
+
+            if write_sparse:
+                s = time.time()
+                with h5py.File(f'sparse/{image_id}.hdf5', 'w') as f:
+                    dset = f.create_dataset('image',
+                                            data=random_image_sequence,
+                                            chunks=chunk_size)
+                    dset[()] = random_image_sequence
+                sparse_writes.append(time.time() - s)
+            s = time.time()
+            dataset.resize(offset, axis=0)
+            dataset[-seq_len:] = random_image_sequence
+            dense_writes.append(time.time() - s)
+
+            if image_id % 100 == 0:
+                if sparse_writes:
+                    print('sparse rolling mean [s]:', sum(sparse_writes[-100:]) / 100)
+                print('dense rolling mean [s]:', sum(dense_writes[-100:]) / 100)
+        dataset.attrs['cum_seq_len'] = cum_seq_lens
+
+    if sparse_writes:
+        with open('sparse_times.pkl', 'wb') as f:
+            pkl.dump(sparse_writes, f)
+
+    with open('dense_times.pkl', 'wb') as f:
+        pkl.dump(dense_writes, f)
